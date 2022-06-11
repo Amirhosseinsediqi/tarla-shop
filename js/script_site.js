bd3abe43d66e9893fc8ba80427a4dae9885d89a5
Amir hossein <87437145+3amin@users.noreply.github.com>
2022-06-12 00:31:06 +0430

    five commit

--- js/script_site.js
+++ js/script_site.js
@@ -1,4 +1,60 @@
+//menu  button ورود/ثبت نام
+$( document ).ready(function() {
+    // Bounce button
+    $('#animatebutton').click(function(){
+        const element =  document.querySelector('.animatebutton');
+        element.classList.add('animated', 'swing');
+        setTimeout(function() {
+          element.classList.remove('swing'); 
+  },        1000); 
+    });
+    
+    
+}); 
 
+//button سبد خرید
+$( document ).ready(function() {
+    // Bounce button
+    $('#animatebutton2').click(function(){
+        const element =  document.querySelector('.animatebutton2');
+        element.classList.add('animated', 'swing');
+        setTimeout(function() {
+          element.classList.remove('swing'); 
+  },        1000); 
+    });
+}); 
+
+
+
+//menu  button ورود/ثبت نام
+$( document ).ready(function() {
+  // Bounce button
+  $('#animatebutton3').click(function(){
+      const element =  document.querySelector('.animatebutton');
+      element.classList.add('animated', 'swing');
+      setTimeout(function() {
+        element.classList.remove('swing'); 
+},        1000); 
+  });
+  
+  
+}); 
+
+//button سبد خرید
+$( document ).ready(function() {
+  // Bounce button
+  $('#animatebutton4').click(function(){
+      const element =  document.querySelector('.animatebutton2');
+      element.classList.add('animated', 'swing');
+      setTimeout(function() {
+        element.classList.remove('swing'); 
+},        1000); 
+  });
+}); 
+
+
+
+//end menu
 
 
 
@@ -67,6 +123,41 @@ $(document).ready(function() {
     $('.card .icon a').toggleClass('active')
   })
 })
+
+
+
+
+
+
+// page login
+
+$(document).ready(function(){
+  $('.login-info-box').fadeOut();
+  $('.login-show').addClass('show-log-panel');
+});
+
+
+$('.login-reg-panel input[type="radio"]').on('change', function() {
+  if($('#log-login-show').is(':checked')) {
+      $('.register-info-box').fadeOut(); 
+      $('.login-info-box').fadeIn();
+      
+      $('.white-panel').addClass('right-log');
+      $('.register-show').addClass('show-log-panel');
+      $('.login-show').removeClass('show-log-panel');
+      
+  }
+  else if($('#log-reg-show').is(':checked')) {
+      $('.register-info-box').fadeIn();
+      $('.login-info-box').fadeOut();
+      
+      $('.white-panel').removeClass('right-log');
+      
+      $('.login-show').addClass('show-log-panel');
+      $('.register-show').removeClass('show-log-panel');
+  }
+});
+
 
 
 
